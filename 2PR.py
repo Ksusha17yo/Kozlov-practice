15a80d9c95275eea1942d8fa625c5f2b7341f7d3
Ksusha17yo <114017747+Ksusha17yo@users.noreply.github.com>
2022-09-22 20:32:44 +0300

    Delete 2PR.py

--- 2PR.py
+++
@@ -1,7 +0,0 @@
-import math
-
-a = float(input("¬ведите a "))
-b = float(input("¬ведите b "))
-c = float(input("¬ведите c "))
-d = (math.pow((b + (math.pow((a - 1), 1./3))), 1./4))/(math.fabs(a - b) * (math.pow(math.sin(c), 2) + math.tan(c)))
-print("s= {0:.6f}".format(d))
